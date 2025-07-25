76e1bd89efce7c1023759019bd886f3af798e228
Nicola Baribeau <nicolabaribeau@gmail.com>
2025-07-24 23:43:25 -0400

    Pipe but 4.5

--- Pipe.gd
+++ Pipe.gd
@@ -1,11 +1,12 @@
-class_name Pipe extends Node
+class_name Pipe
 
+var couplings: Array[Pipe]
 var priority: int = 0
 var base: Variant:
 	set(v):
 		base = v
 		_dirty = false
-var value:
+var value: Variant:
 	get:
 		if _dirty or not value:
 			value = _fill(base)
@@ -18,36 +19,35 @@ func _init(_base: Variant) -> void:
 	base = _base
 	flush()
 
-func _fill(start_value):
-	var couplings = get_children().filter(func(c: Node): return c is Pipe)
-	couplings.sort_custom(func(a: Pipe, b: Pipe): return a.priority < b.priority)
+func _fill(start_value: Variant) -> Variant:
+	couplings.sort_custom(func(a: Pipe, b: Pipe) -> bool: return a.priority < b.priority)
 	for i in range(couplings.size()):
 		var c: Pipe = couplings[i]
-		if c.get_children():
-			var base = c.base
-			var group = c._fill(c.base)
+		if c.couplings.size():
+			var b: Variant = c.base
+			var group: Variant = c._fill(c.base)
 			c.base = group
-			start_value = c.pipe.call(start_value)
-			c.base = base
+			start_value = c.effect.call(start_value)
+			c.base = b
 		else:
-			start_value = c.pipe.call(start_value)
+			start_value = c.effect.call(start_value)
 	return start_value
 
-func effect(value: Variant) -> Variant: return value
+func effect(_value: Variant) -> Variant: return _value
 
 func flush() -> void:
-	for c in get_children():
-		remove_child(c)
-		c.queue_free()
+	for c in couplings:
+		c.free()
+	couplings = []
 	_dirty = true
 
 func add(coupling: Pipe) -> bool:
-	if get_children().has(coupling): return false
-	if coupling.is_inside_tree(): coupling = coupling.duplicate()
-	add_child(coupling)
+	if couplings.find(coupling) == -1: return false
+	couplings.push_back(coupling)
 	return true
 
-func remove(coupling_name: String) -> bool:
-	var node = find_child(coupling_name, false)
-	if node: remove_child(node)
-	return !!node
+func remove(coupling: Pipe) -> bool:
+	var index := couplings.find(coupling)
+	if index != -1:
+		couplings.remove_at(index)
+	return index != -1
